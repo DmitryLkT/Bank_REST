1f82d55df1bf6c4489bc99a9e32d46d4813c481a
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-16 16:39:00 +0600

    f commit

---
+++ src/test/java/org/lukdt/bank_card_management/BankCardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lukdt.bank_card_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
