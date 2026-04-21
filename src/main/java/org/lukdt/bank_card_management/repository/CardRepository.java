08562a336e95bdab11818e720ebfb28e6b2f505e
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-21 20:14:16 +0600

    added executor

--- src/main/java/org/lukdt/bank_card_management/repository/CardRepository.java
+++ src/main/java/org/lukdt/bank_card_management/repository/CardRepository.java
@@ -1,13 +1,14 @@
 package org.lukdt.bank_card_management.repository;
 
 import org.lukdt.bank_card_management.entity.Card;
-import org.lukdt.bank_card_management.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepository extends JpaRepository<User, Long> {
+public interface CardRepository extends JpaRepository<Card, Long>,
+                                            JpaSpecificationExecutor<Card> {
     Page<Card> findByOwnerId(Long ownerId, Pageable pageable);
 }
