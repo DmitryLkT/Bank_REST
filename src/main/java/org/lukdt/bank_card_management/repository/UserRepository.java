dd7b7e3698a6a05eb1ec6a348619a2b0b19149b6
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-21 20:14:44 +0600

    added user repos

---
+++ src/main/java/org/lukdt/bank_card_management/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.lukdt.bank_card_management.repository;
+
+import org.lukdt.bank_card_management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
