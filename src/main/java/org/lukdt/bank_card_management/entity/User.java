eea1dfcfdf1eace38edb2d7bd503c99e044cfccc
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-21 20:18:18 +0600

    User.class implements UserDetails

--- src/main/java/org/lukdt/bank_card_management/entity/User.java
+++ src/main/java/org/lukdt/bank_card_management/entity/User.java
@@ -1,10 +1,16 @@
 package org.lukdt.bank_card_management.entity;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @Column(name="id", nullable = false)
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -20,7 +26,17 @@ public class User {
     private int age;
 
     @Column(name="role", nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Column(name="login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name="password", nullable = false, unique = true)
+    private String password;
+
+    @Column(name="locked", nullable = false)
+    private boolean locked;
 
     public User() {}
 
@@ -29,6 +45,42 @@ public class User {
         this.surname = surname;
         this.age = age;
         this.role = role;
+        this.locked = false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public Long getId() {
@@ -65,5 +117,17 @@ public class User {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
     }
 }
