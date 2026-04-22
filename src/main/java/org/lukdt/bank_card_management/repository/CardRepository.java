62d1d69f5c233e9d0ecdc0fca4c79ee3420db800
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-22 13:51:43 +0600

    added method findByOwnerId

--- src/main/java/org/lukdt/bank_card_management/repository/CardRepository.java
+++ src/main/java/org/lukdt/bank_card_management/repository/CardRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long>,
                                             JpaSpecificationExecutor<Card> {
     Page<Card> findByOwnerId(Long ownerId, Pageable pageable);
+
+    Optional<Card> findByOwnerId(Long ownerId);
 }
