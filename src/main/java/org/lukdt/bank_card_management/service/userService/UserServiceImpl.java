f09814feae77929b6099c6b0a1371bb502cb344f
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-22 11:59:57 +0600

    created methods register and login

--- src/main/java/org/lukdt/bank_card_management/service/userService/UserServiceImpl.java
+++ src/main/java/org/lukdt/bank_card_management/service/userService/UserServiceImpl.java
@@ -1,20 +1,62 @@
 package org.lukdt.bank_card_management.service.userService;
 
+import org.lukdt.bank_card_management.dto.authentication.LoginRequest;
+import org.lukdt.bank_card_management.dto.authentication.RegisterRequest;
+import org.lukdt.bank_card_management.entity.Role;
 import org.lukdt.bank_card_management.entity.User;
+import org.lukdt.bank_card_management.exception.customException.UserAlreadyExistsException;
 import org.lukdt.bank_card_management.repository.UserRepository;
+import org.lukdt.bank_card_management.security.jwt.JwtService;
 import org.lukdt.bank_card_management.service.userService.userServiceInterface.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @Override
     public boolean existsById(Long userId) {
         return userRepository.existsById(userId);
     }
+
+    @Override
+    @Transactional
+    public void register(RegisterRequest request) {
+        if(!userRepository.existsByLogin(request.getLogin())) {
+            throw new UserAlreadyExistsException(request.getLogin());
+        }
+
+        userRepository.save(new User(
+                request.getName(),
+                request.getSurname(),
+                request.getAge(),
+                Role.USER
+        ));
+    }
+
+    @Override
+    public String login(LoginRequest request) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword())
+        );
+
+        UserDetails user = (UserDetails) auth.getPrincipal();
+
+        return jwtService.generateToken(user);
+    }
+
+
 }
--- src/main/java/org/lukdt/bank_card_management/service/userService/userServiceInterface/UserService.java
+++ src/main/java/org/lukdt/bank_card_management/service/userService/userServiceInterface/UserService.java
@@ -1,7 +1,15 @@
 package org.lukdt.bank_card_management.service.userService.userServiceInterface;
 
+import org.lukdt.bank_card_management.dto.authentication.LoginRequest;
+import org.lukdt.bank_card_management.dto.authentication.RegisterRequest;
+import org.lukdt.bank_card_management.entity.User;
+
 public interface UserService {
     boolean existsById(Long userId);
+
+    void register(RegisterRequest request);
+
+    String login(LoginRequest request);
 
 //    Page<CardResponse> getUserCards(Long userId, String query, Pageable pageable);
 //
