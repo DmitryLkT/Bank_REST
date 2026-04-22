87a9c2cfeb189aff21d64a8bffcf34378ea7b427
DmitryTL <Dmitry.plus1@yandex.ru>
2026-04-22 11:57:28 +0600

    added method exists by login

--- src/main/java/org/lukdt/bank_card_management/repository/UserRepository.java
+++ src/main/java/org/lukdt/bank_card_management/repository/UserRepository.java
@@ -2,10 +2,12 @@ package org.lukdt.bank_card_management.repository;
 
 import org.lukdt.bank_card_management.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByLogin(String login);
-    boolean existsLogin(String login);
+    boolean existsByLogin(String login);
 }
